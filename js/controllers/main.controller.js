a6f8b48b17caf663f22c1b207d7f4e3612fe2af4
Kaio Silveira <kaiosilveira@Kaios-MacBook-Pro.local>
2018-01-17 07:02:31 -0200

    Angular added

---
+++ js/controllers/main.controller.js
@@ -0,0 +1,16 @@
+(function() {
+
+  'use strict';
+
+  angular
+  .module('cvc-app')
+  .controller('mainController', mainController);
+
+  function mainController() {
+
+    var vm = this;
+
+  }
+
+
+})();
