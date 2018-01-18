6eb4cecc7320fcaf7a69bb6fa0ead8a25d4e64fd
Kaio Silveira <kaiosilveira@Kaios-MacBook-Pro.local>
2018-01-18 08:09:08 -0200

    Scripts and stylesheets minified

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,15 @@
 var gulp = require('gulp');
 var clean = require('gulp-clean');
+var uglify = require('gulp-uglify');
 var minify = require('gulp-minify');
+let cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
 
 gulp.task('default', function() {
   done();
 });
 
-gulp.task('compress', function() {
-  gulp.src('lib/*.js')
-    .pipe(minify({
-        ext:{
-            src:'-debug.js',
-            min:'.js'
-        },
-        exclude: ['tasks'],
-        ignoreFiles: ['.combo.js', '-min.js']
-    }))
-    .pipe(gulp.dest('dist'))
-});
-
+/* Javascript */
 gulp.task('clean-scripts', function () {
   return gulp.src('dist/js/**/*.js', {read: false})
     .pipe(clean());
@@ -26,7 +17,30 @@ gulp.task('clean-scripts', function () {
 
 gulp.task('scripts', ['clean-scripts'], function () {
   gulp.src('src/js/**/*.js')
+    .pipe(uglify())
+    .pipe(minify({
+        ext:{
+            src:'.js',
+            min:'.min.js'
+        },
+        ignoreFiles: ['-min.js', '.min.js']
+    }))
     .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('default', ['scripts']);
+/* CSS */
+gulp.task('clean-stylesheets', function () {
+  return gulp.src('dist/css/**/*.css', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('stylesheets', ['clean-stylesheets'], function() {
+  gulp.src('src/css/**/*.css')
+  .pipe(cleanCSS({compatibility: 'ie9'}))
+  .pipe(rename({
+      suffix: '.min'
+  }))
+  .pipe(gulp.dest('dist/css'));
+})
+
+gulp.task('default', ['scripts', 'stylesheets']);
